1056676c9144a50e7d9729e920cb7c99719f13c2
Jhon <perezjhon2010@alumnos.cei.es>
2026-03-28 19:48:48 +0100

    feat: add logo header linking to home page

--- rickAndMorty-React/src/components/Nav.jsx
+++ rickAndMorty-React/src/components/Nav.jsx
@@ -1,9 +1,10 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import ThemeToggle from '@/components/ThemeToggle'
 
 const Nav = () => {
     return (
         <header className="Header">
+            <Link to="/" className="Header-logo">Rick & Morty</Link>
             <nav className="Nav">
                 <NavLink to="/personajes">Personajes</NavLink>
                 <NavLink to="/lugares">Lugares</NavLink>
